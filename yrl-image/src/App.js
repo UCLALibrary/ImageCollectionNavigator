a07f528ba22c9de1dbe8c27333495096df2a3a9b
Calvin <calvinha721@gmail.com>
2018-04-25 19:13:35 -0700

    ReactJS files

---
+++ yrl-image/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar.js';
+import Jumbotron from './Jumbotron.js';
+import Card from './Card.js';
+import {Button, Container, Row, Col} from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Jumbotron/>
+
+        <Container className="CardsContainer">
+          <Row >
+            <Col md='4' className= "Cards"> <Card/> </Col>
+            <Col md='4' className= "Cards"> <Card/> </Col>
+            <Col md='4' className= "Cards"> <Card/> </Col>
+          </Row>
+        </Container>
+
+        <Container>
+          <Row className="footer">
+            <Col lg ='11'> </Col>
+            <Col lg='1'className="ViewIcon"> <img src={logo} className="View-Changer"/> </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
