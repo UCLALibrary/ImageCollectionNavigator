2412714abfd20acfe5035ab333650d8dc1c8842f
jonathanchu78 <jonathanchu78@gmail.com>
2018-05-30 20:29:52 -0700

    doesn't completely work

--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -5,25 +5,16 @@ import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle } from 'reactstrap';
 import {Button, Container, Row, Col} from 'reactstrap';
 
-const imageUrls = [
-	"https://www.koreaninamerica.com/wp-content/uploads/2016/10/header-blog-UCLA-tour-690x460.jpg",
-	"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/The_University_of_California_UCLA.svg/1200px-The_University_of_California_UCLA.svg.png",
-	"https://assets3.thrillist.com/v1/image/1296396/size/tmg-article_default_mobile.jpg",
-	"http://waterandpower.org/Historical_DWP_Photo_Collection_LA_Public_Library/UCLA_34.jpg",
-	"http://hauteliving.com/wp-content/uploads/2013/11/ucla-img.jpg",
-	"http://waterandpower.org/Historical_DWP_Photo_Collection_LA_Public_Library/UCLA_Opening_Day_1929.jpg"
-];
+var imageTitles = [];
+var imageURLs = [];
 
 class Gallery extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-  }
+    this.state = {imageURLs: []};
 
-  render(event) {
-
-    var url = "http://172.31.93.116";
+    var url = "http://172.30.12.205";
     var port = "3000";
     var route = "/api/imgs/";
     var request = url + ":" + port + route + this.props.location.search.slice(7);
@@ -32,17 +23,23 @@ class Gallery extends Component {
     .then(results => {
       return results.json();
     }).then(data => {
-      let results = data;
-      console.log(results);
-      this.setState({value: results});
-      alert(results);
+    	imageTitles = data;
+    	var len = imageTitles.length;
+	  	for (var index = 0; index < len; index++){
+	  		imageURLs.push("images/" + imageTitles[index]);
+	  	}
+	    console.log(imageURLs);
+    	this.setState({imageURLs: imageURLs});
     })
+    console.log(imageURLs);
+  }
 
+  render(event) {
     return (
       <div className= "gallery">
       	<h3>Gallery</h3>
       		<Row>
-				{ this.event.state.map(imageUrl =>
+				{ imageURLs.map(imageUrl =>
 					<Col md='4' className= "Cards">
 						<CardImg top width="100%"
 							src={imageUrl}
