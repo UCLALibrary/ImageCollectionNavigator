5bdaab8a5a7d4361796088783522ffaf891d6393
jonathanchu78 <jonathanchu78@gmail.com>
2018-06-06 19:54:56 -0700

    DONE

--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -4,88 +4,102 @@ import logo from './logo.svg';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle } from 'reactstrap';
 import {Button, Container, Row, Col} from 'reactstrap';
+import './Gallery.css';
 
 var imageTitles = [];
-var new_tags_str = "";
+var imageURLs = [];
+var all_tags = [];
 
-var url = "http://172.30.15.25";
+var url = "http://172.30.14.28";
 var port = "3000";
-
-var imgs_route = "/api/imgs/";
 var tags_route = "/api/tags/";
+var imgs_route = "/api/imgs/";
 
 class Gallery extends Component {
 
   constructor(props) {
     super(props);
     this.state = {imgurls: []};
-    var tags = this.props.location.search.slice(7).split('-');
-    
-    console.log(this.props.location.search.slice(7));
-    console.log(tags);
 
-    var imageURLs = [];
-
-    for (var ind= 0; ind < tags.length; ind++) {
-      var request = url + ":" + port + imgs_route + tags[ind];
-      // console.log(request);
-      fetch(request)
-        .then(results => {
-          return results.json();
-        }).then(data => {
-          imageTitles = data;
-          var len = imageTitles.length;
-          for (var index = 0; index < len; index++) {
-            var url = process.env.PUBLIC_URL + '/images/' + imageTitles[index]
-            if (!imageURLs.includes(url))
-              imageURLs.push(url);
-              console.log(imageURLs);
-          }
-          console.log(imageURLs);
-          this.setState({imgurls: imageURLs});
-        })
-    }
-  }
-
-  getAllTags(img) {
-    this.state = {new_tags: []};
-    console.log(img);
-    var img_name = img.split('/')[2];
-    var request = url + ":" + port + tags_route + img_name;
+    var request = url + ":" + port + imgs_route + this.props.location.search.slice(7);
 
     fetch(request)
     .then(results => {
       return results.json();
     }).then(data => {
-      new_tags_str = data.join('-');
-      console.log(new_tags_str);
-
+    	imageTitles = data;
+    	var len = imageTitles.length;
+	  	for (var index = 0; index < len; index++){
+	  		imageURLs.push(process.env.PUBLIC_URL + '/images/' + imageTitles[index]);
+	  	}
+	    console.log(imageURLs);
+      console.log("hello");
+    	this.setState({imgurls: imageURLs});
     })
+    //console.log(imageURLs);
+    //console.log(this.state.imgurls)
 
   }
+
+
+     getAllTags(img) {
+      //this.state = {new_tags: []};
+      console.log(img);
+      var img_name = img.split('/')[2];
+      var request = url + ":" + port + tags_route + img_name;
+      
+      fetch(request)
+      .then(results => {
+        return results.json();
+      }).then(data => {
+        //new_tags_str = data.join('-');
+        //console.log(new_tags_str);
+        console.log(data);
+
+        for (var ind = 0; ind < data.length; ind++){
+          var tag = data[ind];
+          var request2 = url + ':' + port + imgs_route + tag;
+
+          fetch(request2)
+          .then(results => {
+            return results.json();
+          }).then(data2 => {
+            //console.log(data2);
+            var url_list = this.state.imgurls;
+            for (var ind2 = 0; ind2 < data2.length; ind2++){
+              var one_url = process.env.PUBLIC_URL + '/images/' + data2[ind2];
+              if (!url_list.includes(one_url))
+                url_list.unshift(one_url);
+              this.setState({imgurls: url_list});
+            }
+          })
+
+        }
+
+      })
+
+    }
 
   render(event) {
     console.log(this.state.imgurls);
     return (
       <div className= "gallery">
-      	<h3>Gallery</h3>
+        <div className="top">
+          <h1>Image Collection Navigation</h1>
+          <p>Click on an image to find related images</p>
+        </div>
       		<Row>
-				{ this.state.imgurls.map(imageUrl =>
-					<Col md='4' className= "Cards">
-						<Link to={{
-              pathname: '/Gallery',
-              search: '?query=' + {new_tags_str}}}>
-                <CardImg width="100%"
+    				{ this.state.imgurls.map(imageUrl =>
+    					<Col md='4' className= "Cards">
+    						<CardImg width="100%"
     							src={imageUrl}
     							alt="Card image cap"
     							key={imageUrl}
-                  onClick={(e) => this.getAllTags(imageUrl, e)}
-                />
-            </Link>
-					</Col>
-					)
-				}
-			</Row>
+                  onClick={(e) => this.getAllTags(imageUrl, e)}/>
+    					</Col>
+    					)
+    				}
+    			</Row>
       </div>
     );
   }
